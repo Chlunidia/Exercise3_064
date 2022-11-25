eb020c653b1ac03a80baf264eb1bc2297232b0cd
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-11-25 16:13:48 +0700

    Create method Search to search the specified node

--- Exercise3_064/Program.cs
+++ Exercise3_064/Program.cs
@@ -15,5 +15,21 @@ namespace Exercise_Linked_List_A
     class CircularList
     {
         Node LAST;
+        public CircularList()
+        {
+            LAST = null;
+        }
+        public bool Search(int rollNo, ref Node previous, ref Node current) /*searches for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true); /*returns true if the node is found*/
+            }
+            if (rollNo == LAST.rollNumber) /*if the node is present at the end*/
+                return true;
+            else
+                return (false); /*returns false if the node is not found*/
+        }
     }
 }
