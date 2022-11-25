3fd995a96332aac0b0b4997bd19116354b15ec4f
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-11-25 16:08:01 +0700

    Create variable rollNumber, name, next

--- Exercise3_064/Program.cs
+++ Exercise3_064/Program.cs
@@ -7,6 +7,9 @@ namespace Exercise_Linked_List_A
 {
     class Node
     {
-        
+        /*creates Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
     } 
 }
