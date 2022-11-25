70559b61f1ce006c16be660919ea3130d31274ed
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-11-25 16:09:29 +0700

    Create class CircularList

--- Exercise3_064/Program.cs
+++ Exercise3_064/Program.cs
@@ -11,5 +11,9 @@ namespace Exercise_Linked_List_A
         public int rollNumber;
         public string name;
         public Node next;
-    } 
+    }
+    class CircularList
+    {
+        
+    }
 }
